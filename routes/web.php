9996c785160a2715929fb2a7dc4a1fdbc71b9d45
root <raphaeloliveira@golmobile.com.br>
2018-06-17 10:59:37 -0300

    Primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::get('/login', 'AdminAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'AdminAuth\LoginController@login');
+  Route::post('/logout', 'AdminAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'AdminAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'AdminAuth\RegisterController@register');
+
+  Route::post('/password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'employee'], function () {
+  Route::get('/login', 'EmployeeAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'EmployeeAuth\LoginController@login');
+  Route::post('/logout', 'EmployeeAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'EmployeeAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'EmployeeAuth\RegisterController@register');
+
+  Route::post('/password/email', 'EmployeeAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'EmployeeAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'EmployeeAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'EmployeeAuth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'customer'], function () {
+  Route::get('/login', 'CustomerAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'CustomerAuth\LoginController@login');
+  Route::post('/logout', 'CustomerAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'CustomerAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'CustomerAuth\RegisterController@register');
+
+  Route::post('/password/email', 'CustomerAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'CustomerAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'CustomerAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'CustomerAuth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'subscriber'], function () {
+  Route::get('/login', 'SubscriberAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'SubscriberAuth\LoginController@login');
+  Route::post('/logout', 'SubscriberAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'SubscriberAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'SubscriberAuth\RegisterController@register');
+
+  Route::post('/password/email', 'SubscriberAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'SubscriberAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'SubscriberAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'SubscriberAuth\ResetPasswordController@showResetForm');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
